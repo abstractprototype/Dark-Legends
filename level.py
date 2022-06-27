9f15a8f89373d8fe9e53ade212b12265ed876736
abstractprototype <samchenexe@gmail.com>
2022-06-27 04:47:33 -0700

    finished creating the player
    added player movement and collisions

---
+++ level.py
@@ -0,0 +1,41 @@
+import enum
+import pygame
+
+from settings import TILESIZE, WORLD_MAP
+from tile import Tile
+from player import Player
+from debug import debug
+
+
+class Level:
+    def __init__(self):
+
+        # get the display surface
+        self.display_surface = pygame.display.get_surface()
+
+        # sprite group setup
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
+
+        # sprite setup
+        self.create_map()
+
+    def create_map(self):
+        # loops thru all the rows and its indexes
+        for row_index, row in enumerate(WORLD_MAP):
+            # loops thru all the columns and its indexes
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites,
+                         self.obstacle_sprites])
+                if col == 'p':
+                    self.player = Player(
+                        (x, y), [self.visible_sprites], self.obstacle_sprites)
+
+    def run(self):
+        # update and draw the game
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+        debug(self.player.direction)
