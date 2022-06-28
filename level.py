edadfc6b6430cd1ba087d9997d35eb545ea00e01
abstractprototype <samchenexe@gmail.com>
2022-06-27 21:17:47 -0700

    Added Tilemap

--- level.py
+++ level.py
@@ -20,18 +20,21 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        # loops thru all the rows and its indexes
-        for row_index, row in enumerate(WORLD_MAP):
-            # loops thru all the columns and its indexes
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x, y), [self.visible_sprites,
-                         self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player(
-                        (x, y), [self.visible_sprites], self.obstacle_sprites)
+        # # loops thru all the rows and its indexes
+        # for row_index, row in enumerate(WORLD_MAP):
+        #     # loops thru all the columns and its indexes
+        #     for col_index, col in enumerate(row):
+        #         x = col_index * TILESIZE
+        #         y = row_index * TILESIZE
+        #         if col == 'x':
+        #             Tile((x, y), [self.visible_sprites,
+        #                  self.obstacle_sprites])
+        #         if col == 'p':
+        #             self.player = Player(
+        #                 (x, y), [self.visible_sprites], self.obstacle_sprites)
+
+        self.player = Player(
+            (2000, 1430), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         # update and draw the game
@@ -49,10 +52,19 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        # creating the floor
+        self.floor_surf = pygame.image.load(
+            './graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft=(0, 0))
+
     def custom_draw(self, player):
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
 
         # for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
