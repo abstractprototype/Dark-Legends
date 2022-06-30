ac5c0ad35116cb38ee4096fb02d30841b5d8304d
abstractprototype <samchenexe@gmail.com>
2022-06-30 02:28:06 -0700

    finished UI

--- ui.py
+++ ui.py
@@ -16,6 +16,20 @@ class UI:
         self.energy_bar_rect = pygame.Rect(
             10, 34, ENERGY_BAR_WIDTH, BAR_HEIGHT)
 
+        # convert weapon dictionary into a list of graphic only
+        # we do this because we do not want the entire weapon dictionary
+        self.weapon_graphics = []
+        for weapon in weapon_data.values():
+            path = weapon['graphic']
+            weapon = pygame.image.load(path).convert_alpha()
+            self.weapon_graphics.append(weapon)
+
+        # convert magic dictionary
+        self.magic_graphics = []
+        for magic in magic_data.values():
+            magic = pygame.image.load(magic['graphic']).convert_alpha()
+            self.magic_graphics.append(magic)
+
     def show_bar(self, current, max_amount, bg_rect, color):
         # draw bg
         pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
@@ -36,8 +50,46 @@ class UI:
     def show_exp(self, exp):
         # takes in info, AntiAliasing(AA), color
         text_surf = self.font.render(str(int(exp)), False, TEXT_COLOR)
-
+        # location of exp bar
+        x = self.display_surface.get_size()[0] - 20
+        y = self.display_surface.get_size()[1] - 20
         text_rect = text_surf.get_rect(bottomright=(x, y))
+
+        # drawing the exp bar with background and border
+        pygame.draw.rect(self.display_surface,
+                         UI_BG_COLOR, text_rect.inflate(20, 20))
+        self.display_surface.blit(text_surf, text_rect)
+        pygame.draw.rect(self.display_surface,
+                         UI_BORDER_COLOR, text_rect.inflate(20, 20), 3)
+
+    def selection_box(self, left, top, has_switched):
+        bg_rect = pygame.Rect(left, top, ITEM_BOX_SIZE, ITEM_BOX_SIZE)
+        pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
+        if has_switched:
+            pygame.draw.rect(self.display_surface,
+                             UI_BORDER_COLOR_ACTIVE, bg_rect, 3)  # highlights the border yellow when weapon/magic switched
+        else:
+            pygame.draw.rect(self.display_surface, UI_BORDER_COLOR,
+                             bg_rect, 3)  # default black border
+        return bg_rect
+
+    def weapon_overlay(self, weapon_index, has_switched):
+        # weapon #placed in the center of the weapon rectangle
+        bg_rect = self.selection_box(10, 630, has_switched)
+        # grab the weapon index from the weapon graphic list
+        weapon_surf = self.weapon_graphics[weapon_index]
+        # places the weapon in the center of the box
+        weapon_rect = weapon_surf.get_rect(center=bg_rect.center)
+        self.display_surface.blit(weapon_surf, weapon_rect)
+
+    def magic_overlay(self, magic_index, has_switched):
+        # magic #placed in the center of the magic rectangle
+        bg_rect = self.selection_box(80, 635, has_switched)
+        # grab the magic index from the magic graphic list
+        magic_surf = self.magic_graphics[magic_index]
+        # places the magic in the center of the box
+        magic_rect = magic_surf.get_rect(center=bg_rect.center)
+        self.display_surface.blit(magic_surf, magic_rect)
 
     def display(self, player):
         self.show_bar(
@@ -45,4 +97,8 @@ class UI:
         self.show_bar(
             player.energy, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
 
-        self.show_exp()
+        self.show_exp(player.exp)
+
+        # add a "not" to reverse the color switching when weapon is switched
+        self.weapon_overlay(player.weapon_index, not player.can_switch_weapon)
+        self.magic_overlay(player.magic_index, not player.can_switch_magic)
